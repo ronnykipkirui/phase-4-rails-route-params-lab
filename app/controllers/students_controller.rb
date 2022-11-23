143e576f93d9ecdfce9786ad5f517dc8cf8ea0eb
Ronny <ronnybet41@gmail.com>
2022-11-23 09:17:37 +0300

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
+
+
 class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    unless params["name"].nil?
+      name = params["name"].capitalize()
+      students = Student.where("first_name = ? OR last_name = ?","#{name}","#{name}")
+      puts "in cond"
+    end
     render json: students
+  end
+  def show
+    render json: Student.find(params[:id])
   end
 
 end
